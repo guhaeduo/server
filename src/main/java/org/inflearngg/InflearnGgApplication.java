c50fc2a5afea174c35ddce9f9627485acf506f82
sinchangho <vkdghckdh@gmail.com>
2024-01-22 16:53:26 +0900

    created: spring 초기 세팅

---
+++ src/main/java/org/inflearngg/InflearnGgApplication.java
@@ -0,0 +1,13 @@
+package org.inflearngg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnGgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InflearnGgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/inflearngg/InflearnGgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.inflearngg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnGgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
