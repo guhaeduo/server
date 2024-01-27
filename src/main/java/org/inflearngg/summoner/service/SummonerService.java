d825f85794cb25271319e81ac51621d92dba4453
sinchangho <vkdghckdh@gmail.com>
2024-01-27 18:22:43 +0900

    [style] API_key 별도 처리, 코드 정리

--- src/main/java/org/inflearngg/summoner/service/SummonerService.java
+++ src/main/java/org/inflearngg/summoner/service/SummonerService.java
@@ -1,26 +1,22 @@
 package org.inflearngg.summoner.service;
 
 import lombok.RequiredArgsConstructor;
-import org.inflearngg.match.dto.response.MatchingResponseDto;
-import org.inflearngg.match.repository.MatchRepository;
-import org.inflearngg.summoner.dto.response.SummonerResponseDto;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SummonerService {
 
-    // 추후 별도 처리 필요
-    private String API_KEY = "RGAPI-4368f3ed-e6f2-425f-89a5-2ac000869dbc";
+    @Value("${spring.api.key}")
+    private String API_KEY;
     String API_URL = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/{puuid}?api_key={api_key}";
     String API_URL_RANK = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/{summonerId}?api_key={api_key}";
 
     // riotAPI 요청으로 소환사 정보 가져오기
-    private RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate = new RestTemplate();
 
     //api 요청 횟수 제한
 
@@ -28,26 +24,25 @@ public class SummonerService {
         try {
             ResponseEntity<SummonerInfo.SummonerBasicInfo> summonerEntity = restTemplate.getForEntity(API_URL, SummonerInfo.SummonerBasicInfo.class, puuid, API_KEY);
 
-            if(summonerEntity.getStatusCode().is2xxSuccessful()) {
+            if (summonerEntity.getStatusCode().is2xxSuccessful()) {
                 return summonerEntity.getBody();
-            }
-            else {
+            } else {
                 throw new RuntimeException("소환사 정보를 가져오는데 실패했습니다.");
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException("API 요청 중 오류가 발생 했습니다.", e);
         }
     }
+
     public SummonerInfo.RankInfo[] getRankData(String SummonerId) {
         try {
             ResponseEntity<SummonerInfo.RankInfo[]> rankDataEntity = restTemplate.getForEntity(API_URL_RANK, SummonerInfo.RankInfo[].class, SummonerId, API_KEY);
-            if(rankDataEntity.getStatusCode().is2xxSuccessful()) {
+            if (rankDataEntity.getStatusCode().is2xxSuccessful()) {
                 return rankDataEntity.getBody();
-            }
-            else {
+            } else {
                 throw new RuntimeException("소환사 랭크 정보를 가져오는데 실패했습니다.");
             }
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new RuntimeException("API 요청 중 오류가 발생 했습니다.", e);
         }
     }
