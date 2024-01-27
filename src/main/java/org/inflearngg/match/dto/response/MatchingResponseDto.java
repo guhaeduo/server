d91d9fc407887030a4a827af7ac98d5110cc8278
sinchangho <vkdghckdh@gmail.com>
2024-01-27 18:24:29 +0900

    [fix] front에 응답할 데이터 클래스 - 필드 타입 수정

--- src/main/java/org/inflearngg/match/dto/response/MatchingResponseDto.java
+++ src/main/java/org/inflearngg/match/dto/response/MatchingResponseDto.java
@@ -1,13 +1,16 @@
 package org.inflearngg.match.dto.response;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.List;
 
 public class MatchingResponseDto {
+
     @Getter
     @Setter
+    @NoArgsConstructor
     public static class MatchData {
         private Info info;
         private SummonerMatchData currentSummonerMatchData;
@@ -16,6 +19,7 @@ public class MatchingResponseDto {
     }
     @Getter
     @Setter
+    @NoArgsConstructor
     public class Info {
         private int gameDuration;
         private int gameEndStamp;
@@ -25,6 +29,8 @@ public class MatchingResponseDto {
 
         // getters and setters
     }
+
+    
     @Getter
     @Setter
     public static class Team {
@@ -32,6 +38,7 @@ public class MatchingResponseDto {
         private int totalDeath;
         private int totalAssists;
         private int totalGold;
+        private int teamMaxDamage;
         private Objectives objectives;
         private List<SummonerMatchData> participants;
         private boolean isWin;
@@ -43,7 +50,7 @@ public class MatchingResponseDto {
     }
     @Getter
     @Setter
-    public class Objectives {
+    public static class Objectives {
         private int baron;
         private int dragon;
         private int horde;
@@ -56,26 +63,26 @@ public class MatchingResponseDto {
     @Getter
     @Setter
     public static class SummonerMatchData {
-        private int kill;
-        private boolean isBot;
-        private int death;
-        private int assists;
-        private int damage;
-        private int minionKill;
-        private String championName;
-        private int championLevel;
-        private String riotName;
-        private String riotTag;
-        private int totalDamage;
-        private int totalGold;
-        private List<Integer> itemNumberList;
-        private Perks perks;
-        private int visionWards;
-        private int visionScore;
-        private int wardPlaced;
-        private int spell1Id;
-        private int spell2Id;
-        private int pUuid;
+            private int kill;
+            private boolean isBot;
+            private int death;
+            private int assists;
+            private int damage;
+            private int minionKill;
+            private String championName;
+            private int championLevel;
+            private String riotName;
+            private String riotTag;
+            private int totalDamage;
+            private int totalGold;
+            private List<Integer> itemNumberList;
+            private Perks perks;
+            private int visionWards;
+            private int visionScore;
+            private int wardPlaced;
+            private int spell1Id;
+            private int spell2Id;
+            private int pUuid;
 
         // getters and setters
     }
