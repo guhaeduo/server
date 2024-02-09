b47dc1957ad07c484f87c01befc4b83fb66cd4e7
sinchangho <vkdghckdh@gmail.com>
2024-02-09 23:06:05 +0900

    [test] controller 테스트코드 1.1차 작성

--- src/test/java/org/inflearngg/duo/controller/DuoControllerTest.java
+++ src/test/java/org/inflearngg/duo/controller/DuoControllerTest.java
@@ -3,10 +3,12 @@ package org.inflearngg.duo.controller;
 import jakarta.transaction.Transactional;
 import org.inflearngg.duo.dto.request.DuoRequestDto;
 import org.inflearngg.duo.dto.response.DuoResponseDto;
+import org.inflearngg.duo.entity.DuoPost;
 import org.inflearngg.duo.mapper.DuoMapper;
 import org.inflearngg.duo.service.DuoService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -25,6 +27,7 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -76,10 +79,65 @@ class DuoControllerTest {
 
     @Test
     void getDuoPost() {
+
     }
 
     @Test
-    void createDuoPost() {
+    void createDuoPost() throws Exception {
+        String saveDuoPostJson = "{\n" +
+                "  \"postId\": 1,\n" +
+                "  \"riotGameName\": \"test\",\n" +
+                "  \"riotGameTag\": \"test\",\n" +
+                "  \"pUuid\": \"test\",\n" +
+                "  \"isRiotVerified\": false,\n" +
+                "  \"needPosition\": \"test\",\n" +
+                "  \"needQueueType\": \"test\",\n" +
+                "  \"needTier\": \"test\",\n" +
+                "  \"myPosition\": {\n" +
+                "    \"main\": {\n" +
+                "      \"lane\": \"test\",\n" +
+                "      \"championName\": \"test\",\n" +
+                "      \"championIconNumber\": 1\n" +
+                "    },\n" +
+                "    \"sub\": {\n" +
+                "      \"lane\": \"test\",\n" +
+                "      \"championName\": \"test\",\n" +
+                "      \"championIconNumber\": 1\n" +
+                "    }\n" +
+                "  },\n" +
+                "  \"isMicOn\": false,\n" +
+                "  \"memo\": \"test\"\n" +
+                "}";
+        DuoRequestDto.DuoPostSave duoPostSave = new DuoRequestDto.DuoPostSave();
+        duoPostSave.setRiotGameName("test");
+        duoPostSave.setRiotGameTag("test");
+        duoPostSave.setPUuid("test");
+        duoPostSave.setRiotVerified(false);
+        duoPostSave.setNeedPosition("test");
+        duoPostSave.setNeedQueueType("test");
+        duoPostSave.setNeedTier("test");
+        DuoRequestDto.Position position = new DuoRequestDto.Position();
+        DuoRequestDto.Position.Main main = new DuoRequestDto.Position.Main("test", "test", 1);
+        DuoRequestDto.Position.Sub sub = new DuoRequestDto.Position.Sub("test", "test", 1);
+        position.setMain(main);
+        position.setSub(sub);
+        duoPostSave.setMyPosition(position);
+        duoPostSave.setMicOn(false);
+        duoPostSave.setMemo("test");
+        DuoResponseDto.DuoInfo duoInfo = new DuoResponseDto.DuoInfo(1L, "test", "test", "test", false, "test", "test", "test", "test", "test", 1, "test", "test", 1, false, "test");
+        given(duoMapper.duoPostSaveToDuoPost(duoPostSave)).willReturn(new DuoPost());
+        given(duoMapper.duoPostToDuoResponseDtoDuoInfo(new DuoPost())).willReturn(duoInfo);
+        given(duoService.createDuoPost(new DuoPost())).willReturn(new DuoPost());
+        // when
+        ResultActions results = mockMvc.perform(post("/api/duo/post")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(saveDuoPostJson)
+                .characterEncoding("utf-8")
+        );
+        // then
+        results.andExpect(status().isOk())
+                .andDo(print());
+
     }
 
     @Test
