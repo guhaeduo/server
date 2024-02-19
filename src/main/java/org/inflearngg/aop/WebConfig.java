33f587034f88bef030aa1ad6f7ef530ef5d760bf
sinchangho <vkdghckdh@gmail.com>
2024-02-19 17:35:51 +0900

    [fix] AWS cors 에러 해결, 추후 프론트가 배포하면 변경필요

--- src/main/java/org/inflearngg/aop/WebConfig.java
+++ src/main/java/org/inflearngg/aop/WebConfig.java
@@ -8,8 +8,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://183.101.104.237:3000") // 명인님 IP 주소 필요!
+                .allowedOrigins("http://localhost:3000","http://localhost:8080" ) // 명인님 IP 주소 필요!
                 .allowedMethods("*") // 허용할 HTTP 메소드
+                .allowedHeaders("*") // 허용할 커스텀 HTTP 헤더 (Authorization 등)
+                //Access-Control-Allow-Origin:
+                .exposedHeaders("Access-Control-Allow-Origin")
                 .maxAge(3600)
                 .allowCredentials(true);
     }
