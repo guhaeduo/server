8e084e4a3df06ae5be167cc4d4d8e55a5e7c185f
sinchangho <vkdghckdh@gmail.com>
2024-02-20 18:08:32 +0900

    [style] 필요없는 값 제거

--- src/main/java/org/inflearngg/summoner/controller/SummonerController.java
+++ src/main/java/org/inflearngg/summoner/controller/SummonerController.java
@@ -3,15 +3,12 @@ package org.inflearngg.summoner.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.inflearngg.summoner.dto.request.SummonerRequestDto;
-import org.inflearngg.summoner.dto.response.SummonerResponseDto;
 import org.inflearngg.summoner.mapper.SummonerMapper;
 import org.inflearngg.summoner.service.SummonerInfo;
 import org.inflearngg.summoner.service.SummonerService;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import static org.inflearngg.summoner.dto.response.SummonerResponseDto.*;
 
 
 @Slf4j
@@ -25,14 +22,12 @@ public class SummonerController {
 
 
     @GetMapping()
-    public SummonerResponseDto.SummonerData getSummonerInfoByPuuid(
+    public SummonerData getSummonerInfoByPuuid(
             @RequestHeader("region") SummonerRequestDto.Region region,
             @RequestHeader("summonerId") String summonerId) {
-
-//        SummonerInfo.SummonerBasicInfo summonerBasicInfo = summonerService.getSummonerBasicInfo(puuid);
         log.info("region : " + region + " summonerId : " + summonerId);
         SummonerInfo.RankInfo[] rankInfos = summonerService.getRankData(region.name(), summonerId);
-        SummonerResponseDto.SummonerData summonerData = summonerMapper.mapToSummonerResponseDto(rankInfos);
+        SummonerData summonerData = summonerMapper.mapToSummonerResponseDto(rankInfos);
         System.out.println("[정보가져와] 솔랭 : " + summonerData.getSoloRank() + "  자유랭 : " +  summonerData.getFreeRank());
 
         return summonerData;
--- src/main/java/org/inflearngg/summoner/dto/request/SummonerRequestDto.java
+++ src/main/java/org/inflearngg/summoner/dto/request/SummonerRequestDto.java
@@ -17,13 +17,8 @@ public class SummonerRequestDto {
 
     public static enum Region {
         kr, jp1, ph2, sg2, th2, tw2, vn2, na1, br1, la1, la2, oc1, euw1, eun1, ru, tr1;
-//        JP("jp1"),PH("ph2"), SG("sg2"), TH("th2"), TW("tw2"), VN("vn2"), NA("na1"), BR("br1"), LAN("la1"), LAS("la2"), OCE("oc1"), EUW("euw1"), EUNE("eun1"), RU("ru"), TR("tr1");
-//        private String region;
-//        Region(String region) {
-//            this.region = region;
-//        }
-//        public String getRegion() {
-//            return region;
-//        }
+        public String getRegion() {
+            return this.name();
+        }
     }
 }
