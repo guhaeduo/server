3a1735ccd20d9ed2adfdb875f51baa223cb809fe
sinchangho <vkdghckdh@gmail.com>
2024-03-11 13:53:31 +0900

    [feat] 인증 번호 mail전송 기능  setting

---
+++ src/main/java/org/inflearngg/aop/MailConfig.java
@@ -0,0 +1,41 @@
+package org.inflearngg.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+@Slf4j
+@Configuration
+public class MailConfig {
+
+    @Value("${login.naver.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+
+        mailSender.setHost("smtp.naver.com");
+        mailSender.setUsername("guhaeduo@naver.com");
+        mailSender.setPort(465);
+        mailSender.setPassword(password);
+        mailSender.setJavaMailProperties(getMailProperties());
+        return mailSender;
+    }
+
+    // 메일 속성 설정  application.yml에서 해도 됨
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+        return properties;
+    }
+}
