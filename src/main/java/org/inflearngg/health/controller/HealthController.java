dd49039ebedd7c052c2b35e7bf072306bf4d99e2
sinchangho <vkdghckdh@gmail.com>
2024-03-25 18:01:57 +0900

    [experiment] 검증어노테이션 실험

--- src/main/java/org/inflearngg/health/controller/HealthController.java
+++ src/main/java/org/inflearngg/health/controller/HealthController.java
@@ -1,14 +1,56 @@
 package org.inflearngg.health.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Size;
+import lombok.extern.slf4j.Slf4j;
+import org.inflearngg.aop.dto.Region;
+import org.inflearngg.health.request.RequestDto;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 
+@Slf4j
 @RestController
+@Validated
 public class HealthController {
+
 
     @GetMapping("/healthcheck")
     public String healthcheck() {
+        return "OK";
+    }
+
+
+    @PostMapping("/test/valid/post")
+
+    public String postValidCheck(@Valid  @RequestBody RequestDto.ValidCheck validCheck)
+    {
+        log.info("region : {}", validCheck.getRegion());
+//        log.info("age : {}", validCheck.getAge());
+        return "OK";
+    }
+
+    @GetMapping("/test/valid/param")
+    public String getParamValidCheck(@Valid  RequestDto.ValidCheck validCheck)
+    {
+        log.info("region : {}", validCheck.getRegion());
+//        log.info("age : {}", validCheck.getAge());
+        return "OK";
+    }
+
+
+    @GetMapping("/test/valid/header")
+    public String getHeaderValidCheck(@RequestHeader(name = "password") @Size(min = 3, max = 4) String validCheck)
+    {
+        log.info("header : {}", validCheck);
+        return "OK";
+    }
+
+    @Validated
+    @GetMapping("/test/valid/{id}")
+    public String getPathValidCheck(@PathVariable("id") Integer validCheck)
+    {
+        log.info("path : {}", validCheck);
         return "OK";
     }
 }
