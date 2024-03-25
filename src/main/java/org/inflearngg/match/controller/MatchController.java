2895e5952054775944b6d22b8f654b7bff9cd2d7
sinchangho <vkdghckdh@gmail.com>
2024-03-25 18:12:39 +0900

    [fix] valid 제거 -> validated 로 적용

--- src/main/java/org/inflearngg/match/controller/MatchController.java
+++ src/main/java/org/inflearngg/match/controller/MatchController.java
@@ -9,6 +9,7 @@ import org.inflearngg.match.dto.response.MatchingResponseDto;
 import org.inflearngg.match.mapper.ProcessMatchMapper;
 import org.inflearngg.match.service.MatchService;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -31,6 +32,7 @@ public class MatchController {
     //헤더로 puuid, 큐타입 받아서 전적정보 가져오기
     @GetMapping("/list")
     @ResponseBody
+    @Validated
     public MatchingResponseDto getMatchDataListByPuuid(
             @RequestHeader("puuid") String puuid,
             @RequestHeader("queueType") MatchRequestDto.QueueType queueId,
@@ -40,35 +42,25 @@ public class MatchController {
         if (puuid == null) {
             throw new RuntimeException("puuid 헤더 정보가 잘못되었습니다.");
         }
-        LocalDateTime startTime = LocalDateTime.now();
-        log.debug("요청시간 : " + startTime);
         String[] matchIdList = matchService.getMatchIdsByPuuid(puuid, queueId.getQueueId(), region.getContinent());
         MatchingResponseDto matchingResponseDto = new MatchingResponseDto();
         matchingResponseDto.setMatchDataList(mapper.mapToMatchDataList(matchService.getMatchDataList(matchIdList, puuid, region.getContinent())));
         matchingResponseDto.setTotalGameCnt(matchingResponseDto.getMatchDataList().size());
         matchingResponseDto.setQueueType(queueId.name());
 
-        LocalDateTime endTime = LocalDateTime.now();
-        log.debug("응답시간 : " + endTime);
-        log.debug("요청 후 응답시간 : " + startTime.until(endTime, ChronoUnit.SECONDS) + "s");
         return matchingResponseDto;
     }
 
     @GetMapping("/summary")
     @ResponseBody
+    @Validated
     public MatchingResponseDto.SummonerRankInfo getSummonerRankInfo(
             @RequestHeader("puuid") String puuid,
             @RequestHeader("queueType") MatchRequestDto.QueueType queueId,
             @RequestHeader("region") Region region
     ) throws ExecutionException, InterruptedException {
-        LocalDateTime startTime = LocalDateTime.now();
-        log.debug("요청시간 : " + startTime);
         String[] matchIdList = matchService.getMatchIdsByPuuid(puuid, queueId.getQueueId(), region.getContinent());
-        MatchingResponseDto.SummonerRankInfo summonerRankInfo = mapper.mapToSummonerRankInfo(matchService.getSummonerSummaryInfo(matchIdList, puuid, region.getContinent()));
-        LocalDateTime endTime = LocalDateTime.now();
-        log.debug("응답시간 : " + endTime);
-        log.debug("요청 후 응답시간 : " + startTime.until(endTime, ChronoUnit.SECONDS) + "s");
-        return summonerRankInfo;
+        return mapper.mapToSummonerRankInfo(matchService.getSummonerSummaryInfo(matchIdList, puuid, region.getContinent()));
     }
 
 }
