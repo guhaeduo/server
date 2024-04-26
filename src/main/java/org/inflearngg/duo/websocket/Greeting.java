10d427291e1556bd6aa6cb10d2562877bb97cac3
sinchangho <vkdghckdh@gmail.com>
2024-04-26 16:11:48 +0900

    [study] 구독자가 볼 객체, 발행자가 보내는 객체 생성

---
+++ src/main/java/org/inflearngg/duo/websocket/Greeting.java
@@ -0,0 +1,13 @@
+package org.inflearngg.duo.websocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Greeting {
+    private String content;
+}
