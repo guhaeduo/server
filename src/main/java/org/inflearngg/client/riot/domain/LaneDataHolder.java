d86ba3d2c4f9e149972a095a58434ddbc36e4182
sinchangho <vkdghckdh@gmail.com>
2024-08-24 16:56:21 +0900

    [feat] 소환사전적 라인별 통계 정보를 담기위한 비동기 class

---
+++ src/main/java/org/inflearngg/client/riot/domain/LaneDataHolder.java
@@ -0,0 +1,8 @@
+package org.inflearngg.client.riot.domain;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class LaneDataHolder {
+    AtomicInteger laneGameCnt = new AtomicInteger(0);
+    AtomicInteger laneWinCnt = new AtomicInteger(0);
+}
